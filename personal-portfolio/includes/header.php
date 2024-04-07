215582ef8be032e761c8d638866c1a05aeb31797
Wasi khan <wasikhan7099@gmail.com>
2024-04-07 15:53:27 -0700

    first check

---
+++ personal-portfolio/includes/header.php
@@ -0,0 +1,31 @@
+<header>
+	<div class="main-header">
+		<div class="container">
+			<div class="menu-Bar">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="row align-items-center">
+				<div class="col-md-4 text-left">
+					<a href="./" class="logo">
+						<img src="assets/images/logo.png" alt="">
+					</a>
+				</div>
+			
+				<div class="col-md-8 text-right">
+					<div class="menuWrap">
+						<ul class="menu">
+							<li class="active"><a href="./">Home</a></li>
+							<li><a href="#">Service</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Skill</a></li>
+							<li><a href="#">Work</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
